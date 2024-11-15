a62f59df756144f356b4659903804e7eaf9b77d8
Thinakaran <thinakaranmanohar@yahoo.com>
2024-11-15 13:30:34 +0530

    nessage

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/projects/',
+  server: {
+    headers: {
+      'Permissions-Policy': 'fullscreen=(self)' // Remove interest-cohort here
+    }
+  }
 })
